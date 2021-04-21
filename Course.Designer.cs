537d609fe69aff0723f52143cc4812a48a392ad7
Whistick <82952482+Whistick@users.noreply.github.com>
2021-04-22 01:25:44 +0800

    Update Course.Designer.cs

--- Course.Designer.cs
+++ Course.Designer.cs
@@ -6,6 +6,8 @@ namespace WinFormsApp1
         /// <summary>
         /// Required designer variable.
         /// </summary>
+        
+        
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
